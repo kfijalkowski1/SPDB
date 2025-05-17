26dabab872bdd6015a5dc52ab81f5428f700656e
towienko <01169218@pw.edu.pl>
2025-05-17 20:26:28 +0200

    Distinguish road types during import

--- db/osm_imports/import_osm.sh
+++ db/osm_imports/import_osm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-set -e
+set -euo pipefail
+
+set -x
 
 PGHOST=${PG_HOST:-localhost}
 PGUSER=${PG_USER:-postgres}
@@ -31,19 +33,33 @@ else
   psql -U "$PGUSER" -h "$PGHOST" -d "${DB_NAME}" -c "CREATE EXTENSION postgis;"
   psql -U "$PGUSER" -h "$PGHOST" -d "${DB_NAME}" -c "CREATE EXTENSION pgrouting;"
   psql -U "$PGUSER" -h "$PGHOST" -d "${DB_NAME}" -c "CREATE EXTENSION hstore;"
+  psql -U "$PGUSER" -h "$PGHOST" -d "${DB_NAME}" -c "CREATE TYPE road_type_enum AS ENUM ('roads_primary', 'roads_secondary', 'roads_paved', 'roads_unpaved', 'roads_unknown_surface', 'cycleways');"
 
   # Import OSM files using osm2pgrouting
-  for file in "${OSM_FILES[@]}"; do
-    echo "Importing $file..."
-    osm2pgrouting -f "$file" \
-      -d "${DB_NAME}" \
-      -U "$PGUSER" \
-      -h "$PGHOST" \
-      -W "$PGPASSWORD" \
-      -c "/mapconfig_bikes.xml" \
-      --tags \
-      --attributes
+  for source_file in "${OSM_FILES[@]}"; do
+    echo "Processing $source_file..."
+    
+    for filter in "roads_primary" "roads_secondary" "roads_paved" "roads_unpaved" "roads_unknown_surface" "cycleways"; do
+      PARAMS_FILE="/osmfilter/$filter.txt"
+      FILTERED_OSM_FILE="${filter}_filtered.osm"
+
+      echo "Importing $filter from $source_file..."
+      osmfilter $source_file "--parameter-file=$PARAMS_FILE" > "$FILTERED_OSM_FILE"
+
+      osm2pgrouting -f "$FILTERED_OSM_FILE" \
+        -d "${DB_NAME}" \
+        -U "$PGUSER" \
+        -h "$PGHOST" \
+        -W "$PGPASSWORD" \
+        -c "/mapconfig_bikes.xml" \
+        --tags \
+        --attributes \
+
+      psql -U "$PGUSER" -h "$PGHOST" -d "${DB_NAME}" -c "ALTER TABLE ways ADD IF NOT EXISTS road_type road_type_enum DEFAULT NULL;"
+      psql -U "$PGUSER" -h "$PGHOST" -d "${DB_NAME}" -c "UPDATE ways SET road_type = '$filter' WHERE road_type IS NULL;"
+    done
   done
+  psql -U "$PGUSER" -h "$PGHOST" -d "${DB_NAME}" -c "ALTER TABLE ways ALTER COLUMN road_type SET NOT NULL;"
 
   echo "Import completed."
 fi
